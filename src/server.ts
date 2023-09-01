ff52cac0097b27c182284199d5b771c6a6fa8a39
Diego Arruda <arrudadiego1@gmail.com>
2023-09-01 11:44:38 -0300

    Criando o Primeiro Resource

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,19 @@
 import express from "express";
-import { sequelize } from "./database";
+import { sequelize } from "./database/index";
+import { adminJs, adminJsRouter } from "./adminjs/index";
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+app.use(express.static("public"));
 
-app.listen(PORT, () => {
-  sequelize.authenticate().then(() => {
-    console.log(`DB connection successfull`);
+app.use(adminJs.options.rootPath, adminJsRouter);
+
+const PORT = process.env.port || 3000;
+
+app.listen(PORT, async () => {
+  await sequelize.authenticate().then(() => {
+    console.log("DB connection successfull.");
   });
-  console.log(`Server has started successfuly at port ${PORT}`);
+
+  console.log(`Server started successfuly at port ${PORT}.`);
 });
