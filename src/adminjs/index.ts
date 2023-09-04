0b9991364e77e986ac9816b0d5367467ccaa1038
Diego Arruda <arrudadiego1@gmail.com>
2023-09-04 15:22:45 -0300

    Personalizando o dashboard

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -3,7 +3,7 @@ import AdminJsExpress from "@adminjs/express";
 import AdminJsSequelize from "@adminjs/sequelize";
 import { sequelize } from "../database";
 import { adminJsResources } from "./resources";
-import { User } from "../models";
+import { Category, Course, Episode, User } from "../models";
 import bcrypt from "bcrypt";
 import { locale } from "./locale";
 
@@ -33,6 +33,21 @@ export const adminJs = new AdminJs({
         accent: "#151515",
         hoverBg: "#151515",
       },
+    },
+  },
+  dashboard: {
+    component: AdminJs.bundle("./components/Dashboard"),
+    handler: async (req, res, context) => {
+      const courses = await Course.count();
+      const episodes = await Episode.count();
+      const categories = await Category.count();
+      const standardUsers = await User.count({ where: { role: "user" } });
+      res.json({
+        Cursos: courses,
+        Episódios: episodes,
+        Categorias: categories,
+        Usuários: standardUsers,
+      });
     },
   },
 });
