5058d4c2514d4fa5a58836bebc257e7fed33691a
Diego Arruda <arrudadiego1@gmail.com>
2023-09-06 15:50:34 -0300

    Login com JSON Web Token

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { jwtService } from "../services/jwtService";
 import { userService } from "../services/userService";
 
 export const authController = {
@@ -26,6 +27,43 @@ export const authController = {
     } catch (error) {
       if (error instanceof Error) {
         return res.status(400).json({ message: error.message });
+      }
+    }
+  },
+
+  // POST /auth/login
+  login: async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    try {
+      const user = await userService.findByEmail(email);
+
+      if (!user) {
+        return res.status(401).json({ message: "E-mail não registrado" });
+      }
+
+      user.checkPassword(password, (err, isSame) => {
+        if (err) {
+          return res.status(400).json({ message: err.message });
+        }
+
+        if (!isSame) {
+          return res.status(401).json({ message: "Senha incorreta" });
+        }
+
+        const payload = {
+          id: user.id,
+          firstName: user.firstName,
+          email: user.email,
+        };
+
+        const token = jwtService.signToken(payload, "7d");
+
+        return res.json({ authenticated: true, ...payload, token });
+      });
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message });
       }
     }
   },
